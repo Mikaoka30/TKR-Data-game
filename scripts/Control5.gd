e9842f7f36251e5eb917a94351e43228348421f2
ngairetkr <ngaire@kahuiraraunga.io>
2024-06-10 14:31:13 +1200

    Scene 5 Map changes and script

--- scripts/Control5.gd
+++ scripts/Control5.gd
@@ -5,3 +5,23 @@ func _on_visibility_changed():
 	for texture in SocialMediaVars.social_array:
 		var metadata = texture.get_meta("Name")
 		
+		match metadata:
+			"Google":
+				print("This is google")
+				get_node("GLine").visible = true
+				
+			"Facebook":
+				print("This is facebook")
+				get_node("FLine").visible = true
+				
+			"Instagram":
+				print("This is instagram")
+				get_node("ILine").visible = true
+				
+			"Snapchat":
+				print("This is snapchat")
+				get_node("SLine").visible = true
+				
+			"TikTok":
+				print("This is TikTok")
+				get_node("TLine").visible = true
