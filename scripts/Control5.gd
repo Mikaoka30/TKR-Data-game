7eecd24b0bc7b534b9b069059fa62ce052a1ffb6
ngairetkr <ngaire@kahuiraraunga.io>
2024-06-14 14:08:27 +1200

    Changes to Control5 script, adding buttons for map. Created Control6 test layout

--- scripts/Control5.gd
+++ scripts/Control5.gd
@@ -3,19 +3,7 @@ extends Control
 # Display locations on map
 func _on_visibility_changed():
 	
-	var selection_array = SocialMediaVars.social_array
-	
-	#var hBox =  $DisplayHBox
-	#
-	#for selected in selection_array:
-		#var vContainer = VBoxContainer.new()
-		#var textSelected = Label.new()
-		#
-		#textSelected.selected = selected.selected
-		#
-		#vContainer.add_child(textSelected)
-		#hBox.add_child(vContainer)
-		
+	#var selection_array = SocialMediaVars.social_array
 	
 	for texture in SocialMediaVars.social_array:
 		var metadata = texture.get_meta("Name")
@@ -24,22 +12,27 @@ func _on_visibility_changed():
 			"Google":
 				print("This is google")
 				get_node("GLine").visible = true
+				get_node("gbtn").visible = true
 				
 			"Facebook":
 				print("This is facebook")
 				get_node("FLine").visible = true
+				get_node("fbtn").visible = true
 				
 			"Instagram":
 				print("This is Instagram")
 				get_node("ILine").visible = true
+				get_node("ibtn").visible = true
 				
 			"Snapchat":
 				print("This is snapchat")
 				get_node("SLine").visible = true
+				get_node("sbtn").visible = true
 				
 			"TikTok":
 				print("This is TikTok")
 				get_node("TLine").visible = true
+				get_node("tbtn").visible = true
 #
-#Display selected social media elements
+
 
