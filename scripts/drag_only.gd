dbb997aec2c78618e500f637a79fe411c5160b13
Mikaoka30 <mika@kahuiraraunga.io>
2024-06-17 10:14:51 +1200

    Add labels and fix naming conventions of scripts

--- scripts/drag_only.gd
+++ scripts/drag_only.gd
@@ -14,9 +14,3 @@ func _get_drag_data(at_position):
 	set_drag_preview(preview)
 	
 	return texture
-
-func _can_drop_data(at_position, data):
-	return data is Texture2D
-
-func _drop_data(at_position, data):
-	texture = data
--- scripts/drop_area.gd
+++ scripts/drop_area.gd
@@ -1,6 +1,5 @@
 extends TextureRect
 
-
 func _can_drop_data(at_position, data):
 	return data is Texture2D
 
