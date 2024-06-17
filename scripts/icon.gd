5dce2a7b0db5539949b8d3b6795275dda8528a88
ngairetkr <ngaire@kahuiraraunga.io>
2024-06-17 15:24:45 +1200

    Adjusment to the layout of Control3 and Control4

--- scripts/icon.gd
+++ scripts/icon.gd
@@ -1,7 +1,5 @@
 extends TextureRect
 
-
-
 #Function to display elements of the array
 
 var is_selected = false
@@ -9,18 +7,6 @@ var is_selected = false
 func _ready():
 	set_process_input(true) # Enable input processing
 	
-	#display_global_array()
-	
-#@onready var label = $Control5/DisplayArray
-#
-#func display_global_array():
-	#var elements = SocialMediaVars
-	#
-	#var text = ""
-	#for element in elements:
-		#text += element + "\n"		
-		#label.text = text
-
 func _gui_input(event):
 	if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
 		toggle_selected()
@@ -28,7 +14,7 @@ func _gui_input(event):
 func toggle_selected():
 	is_selected = not is_selected
 	if is_selected:
-		self.scale = Vector2(1.2, 1.2) # Increase size when selected
+		self.scale = Vector2(1.05, 1.05) # Increase size when selected 
 		SocialMediaVars.social_array.append(self)
 		print("Logo added")
 	else:
