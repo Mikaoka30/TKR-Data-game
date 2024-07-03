5fb9ff0bb6f7e637ce54b40a8d07b6ae45fa362c
Mikaoka30 <mika@kahuiraraunga.io>
2024-07-04 11:44:20 +1200

    Add coi-service file and docs folder for html export

--- scripts/drag_only.gd
+++ scripts/drag_only.gd
@@ -3,10 +3,11 @@ extends TextureRect
 func _get_drag_data(at_position):
 	
 	var preview_texture = TextureRect.new()
-	
+	#preview of the icon when holding the icon
 	preview_texture.texture = texture
 	preview_texture.expand_mode = 1
 	preview_texture.size = Vector2(100,50)
+
 	
 	var preview = Control.new()
 	preview.add_child(preview_texture)
--- scripts/social_media_icon_select.gd
+++ scripts/social_media_icon_select.gd
@@ -6,17 +6,18 @@ var is_selected = false
 
 func _ready():
 	set_process_input(true) # Enable input processing
-	
+#this is to fire an event when icon is left clicked
 func _gui_input(event):
 	if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
 		toggle_selected()
-
+#When taonga is selected, give a visual confirmation and add to taonga array
 func toggle_selected():
 	is_selected = not is_selected
 	if is_selected:
 		self.scale = Vector2(1.05, 1.05) # Increase size when selected 
 		SocialMediaVars.social_array.append(self)
 		print("Logo added")
+		#continue button is enabled only when a social media is selected
 		$"../Continue3".disabled = false
 	else:
 		var index = SocialMediaVars.social_array.find(self)
