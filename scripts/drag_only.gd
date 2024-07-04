7640860e6e62edd0e7a01bf14de6973f726d7c69
Mikaoka30 <mika@kahuiraraunga.io>
2024-07-04 15:40:08 +1200

    Update view starting point

--- scripts/drag_only.gd
+++ scripts/drag_only.gd
@@ -6,13 +6,14 @@ func _get_drag_data(at_position):
 	#preview of the icon when holding the icon
 	preview_texture.texture = texture
 	preview_texture.expand_mode = 1
-	preview_texture.position = Vector2(-50, -50)
+	preview_texture.position = Vector2(at_position.x, at_position.y)
+
 
 	preview_texture.size = Vector2(100,50)
 
 	var preview = Control.new()
 	preview.add_child(preview_texture)
-	preview.position = Vector2(-50, -50)
+	preview.position = Vector2(at_position.x, at_position.y)
 	set_drag_preview(preview)
 	
 	return texture
