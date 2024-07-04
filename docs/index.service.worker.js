e2fada53224128318805b362c1b0495e32fbc1e9
Mikaoka30 <mika@kahuiraraunga.io>
2024-07-04 15:45:35 +1200

    Slight change to view

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1720064547|10669684652";
+const CACHE_VERSION = "1720064716|10838324979";
 const CACHE_PREFIX = "FirstGame-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
