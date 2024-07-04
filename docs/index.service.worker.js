862f1dd81300d11089d6aecc2590770ab683b7b9
Mikaoka30 <mika@kahuiraraunga.io>
2024-07-04 12:49:57 +1200

    Delete coi-serviceworker and change encription settings

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1720053942|64689432";
+const CACHE_VERSION = "1720054145|267286892";
 const CACHE_PREFIX = "FirstGame-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
